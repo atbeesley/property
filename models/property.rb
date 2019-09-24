8b5f2c6303132214357252befc695ccfae691fd6
user <you@exampalex.com>
2019-09-24 14:47:06 +0100

    save

--- models/property.rb
+++ models/property.rb
@@ -1,8 +1,8 @@
 require('pg')
 
-attr_reader :id, :address, :value, :number_of_bedrooms, :year_built
-
 class Property
+
+  attr_accessor :id, :address, :value, :number_of_bedrooms, :year_built
 
   def initialize( options )
     @id = options['id'].to_i() if options['id']
@@ -13,7 +13,20 @@ class Property
 
   end
 
-  def save
+  def save()
+    db = PG.connect({ dbname: 'property_tracker', host: 'localhost' })
+    sql =
+    "INSERT INTO properties
+    (
+      address,
+      value,
+      number_of_bedrooms,
+      year_built
+      ) VALUES ($1, $2, $3, $4) RETURNING *"
+      values = [@address, @value, @number_of_bedrooms, @year_built]
+      db.prepare("save", sql)
+      @id = db.exec_prepared("save", values)[0]["id"].to_i()
+      db.close()
+    end
 
-
-end
+  end
